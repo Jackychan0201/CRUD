d9c95f9fc6e0cc0c643feaf34582c18e88b1d38a
Jackychan0201 <150186719+Jackychan0201@users.noreply.github.com>
2025-04-05 23:52:52 +0200

    frontend api url changed

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 export const fetchNotes = async () => {
   const response = await axios.get(`${API_URL}/notes`);
